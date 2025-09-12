b909919cdd12bed3cdee251d87d9b39aa2fa4ace
burnysc2 <gamingburny@gmail.com>
2025-09-12 17:21:14 +0200

    Fix paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css'
+	import favicon from '$lib/assets/favicon.svg'
+	import { base } from '$app/paths'
 
-	let { children } = $props();
+	let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="{base}/{favicon}" />
 </svelte:head>
 
 {@render children?.()}
--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,10 @@ const config = {
 			fallback: null,
 			// fallback: 'index.html',
 			precompress: false,
-			strict: true
+			strict: true,
+			paths: {
+				base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+			}
 		})
 	}
 }
