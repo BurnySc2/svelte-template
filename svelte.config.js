90d415d6e063568e7ab15c1fcd3a887fe38ddf61
burnysc2 <gamingburny@gmail.com>
2025-09-12 17:44:58 +0200

    Fix paths location

--- svelte.config.js
+++ svelte.config.js
@@ -6,16 +6,13 @@ const config = {
 	// preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'dist',
-			assets: 'dist',
 			fallback: '404.html',
-			// fallback: 'index.html',
 			precompress: false,
 			strict: true,
-			paths: {
-				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			}
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 }
 
