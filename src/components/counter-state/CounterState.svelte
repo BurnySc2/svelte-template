8aff86ec1b5821ce3f3d3e1aae9ea4a1ee412f1e
burnysc2 <gamingburny@gmail.com>
2025-09-24 18:05:04 +0200

    Use effect to wait till page has loaded

--- src/components/counter-state/CounterState.svelte
+++ src/components/counter-state/CounterState.svelte
@@ -4,6 +4,12 @@
 	let my_counter_state = use_local_storage_state('my_counter_state', 0)
 </script>
 
+<button
+	class="rounded border-2 border-black p-2"
+	onclick={() => {
+		my_counter_state.value += 1
+	}}>Increment</button
+>
 <div>
 	{my_counter_state.value}
 </div>
